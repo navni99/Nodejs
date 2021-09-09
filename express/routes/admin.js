19422726c75189797d79044dd24c06e3c77640f8
navni99 <50635765+navni99@users.noreply.github.com>
2021-09-09 16:03:26 +0530

    Passing data around

--- express/routes/admin.js
+++ express/routes/admin.js
@@ -1,22 +1,21 @@
-//same url for post and get behave differently
-//add-product is internally now treated as /admin/add-product
+//We have products and routes now exported, so need to specify where importing
 
 const express = require('express');
 const path = require('path');
-// const { dirname } = require('path/posix');
 const router = express.Router();
-
+const products = [];
 router.get('/add-product', (req, res, next) => {
-  /*res.send(
-    '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Submit</button></form>'
-  );*/
-  //res.sendFile('../htmlpages/add-product.html');
-  //this will not work because / join is not common across different os
-  res.sendFile(path.join(__dirname, '../', 'htmlpages', 'add-product.html')); //__dirname gives dir name of current parent, we go up one dir n then navigate
+  res.sendFile(path.join(__dirname, '..', 'htmlpages', 'add-product.html'));
 });
 router.post('/add-product', (req, res, next) => {
   console.log(req.body);
+  products.push({ title: req.body.title });
   res.redirect('/');
 });
 
-module.exports = router;
+module.exports = {
+  routes: router,
+  products: products,
+};
+
+//Major disadvantage of passing data like this is it is DATA INHERENT TO NODE SERVER, it will be same for different users across different requests, will update for all n show to all, Unlikely to need this.
--- express/routes/shop.js
+++ express/routes/shop.js
@@ -1,7 +1,10 @@
 const express = require('express');
 
 const router = express.Router();
+
+const adminData = require('./admin');
 router.get('/', (req, res, next) => {
+  console.log('title taken from admin add product', adminData.products);
   res.send('Hello from /');
 });
 module.exports = router;
