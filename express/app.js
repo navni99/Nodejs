d78be37fec78a0115ffff40232131bbc9f9e588b
navni99 <50635765+navni99@users.noreply.github.com>
2021-09-09 14:44:40 +0530

    redirect,bodyParser

--- express/app.js
+++ express/app.js
@@ -1,24 +1,20 @@
-const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
-app.use('/', (req, res, next) => {
-  //use matches anything with / at begining , not / exactly, app.get or app.post matches url exactly
-  console.log('This always runs');
-  next(); // without next we dont move to the second app.use function
-});
+app.use(bodyParser.urlencoded({ extended: false })); //no need to parse manually now
 app.use('/add-product', (req, res, next) => {
   console.log('In add-product');
-  res.send('add product'); //string becomes text/html type automatically
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Submit</button></form>'
+  );
+});
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 app.use('/', (req, res, next) => {
-  // if it was placed before /add-product, add-product would return this and never reach In add product code, when using app.use order of mentioning requests matters because it doesnt match url exactly
-  console.log('In second /');
-  res.send('Hello from second /');
-  // next(); // without next we dont move to the second app.use function
+  res.send('Hello from /');
 });
 
-/*
-const server = http.createServer(app);
-server.listen(3000);
-APP.LISTEN DOES BOTH THESE*/
 app.listen(3000);
