4adedf89722d9c6db14544d200af631f7c0302e8
navni99 <50635765+navni99@users.noreply.github.com>
2021-09-09 15:04:35 +0530

    common path for a route

--- express/app.js
+++ express/app.js
@@ -8,8 +8,8 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
-app.use(shopRoutes); //app.use can be replaced with get if needed
+app.use('/admin', adminRoutes); //admin routes now have a common path of /admin
+app.use(shopRoutes);
 app.use('/', (req, res, next) => {
   res.status(404).send('Page not found'); //sending status, setHeaders can be used like status
 });
--- express/routes/admin.js
+++ express/routes/admin.js
@@ -1,13 +1,16 @@
+//same url for post and get behave differently
+//add-product is internally now treated as /admin/add-product
+
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
   res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Submit</button></form>'
+    '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Submit</button></form>'
   );
 });
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
   console.log(req.body);
   res.redirect('/');
 });
