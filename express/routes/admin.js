ee4cb78cc134030591279d8e02d442a3be41a3dd
navni99 <50635765+navni99@users.noreply.github.com>
2021-09-09 15:37:08 +0530

    response html page

--- express/routes/admin.js
+++ express/routes/admin.js
@@ -2,13 +2,17 @@
 //add-product is internally now treated as /admin/add-product
 
 const express = require('express');
-
+const path = require('path');
+// const { dirname } = require('path/posix');
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-  res.send(
+  /*res.send(
     '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Submit</button></form>'
-  );
+  );*/
+  //res.sendFile('../htmlpages/add-product.html');
+  //this will not work because / join is not common across different os
+  res.sendFile(path.join(__dirname, '../', 'htmlpages', 'add-product.html')); //__dirname gives dir name of current parent, we go up one dir n then navigate
 });
 router.post('/add-product', (req, res, next) => {
   console.log(req.body);
