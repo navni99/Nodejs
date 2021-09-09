3ce07fd2d092733a4a85ed49189b06f73ae8c93c
navni99 <50635765+navni99@users.noreply.github.com>
2021-09-09 14:50:49 +0530

    app.get app.post

--- express/app.js
+++ express/app.js
@@ -3,17 +3,16 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false })); //no need to parse manually now
-app.use('/add-product', (req, res, next) => {
-  console.log('In add-product');
+app.get('/add-product', (req, res, next) => {
   res.send(
     '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Submit</button></form>'
   );
 });
-app.use('/product', (req, res, next) => {
+app.post('/product', (req, res, next) => {
   console.log(req.body);
   res.redirect('/');
 });
-app.use('/', (req, res, next) => {
+app.get('/', (req, res, next) => {
   res.send('Hello from /');
 });
 
