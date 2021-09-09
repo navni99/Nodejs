88c35afb607692b9944e2737381da1d72f633c37
navni99 <50635765+navni99@users.noreply.github.com>
2021-09-09 12:47:43 +0530

    order of using app.use

--- express/app.js
+++ express/app.js
@@ -1,13 +1,20 @@
 const http = require('http');
 const express = require('express');
 const app = express();
-app.use((req, res, next) => {
-  console.log('In middlewear');
+app.use('/', (req, res, next) => {
+  //use matches anything with / at begining , not / exactly, app.get or app.post matches url exactly
+  console.log('This always runs');
   next(); // without next we dont move to the second app.use function
 });
-app.use((req, res, next) => {
-  console.log('In second middlewear');
-  res.send('I am type any'); //string becomes text/html type automatically
+app.use('/add-product', (req, res, next) => {
+  console.log('In add-product');
+  res.send('add product'); //string becomes text/html type automatically
+});
+app.use('/', (req, res, next) => {
+  // if it was placed before /add-product, add-product would return this and never reach In add product code, when using app.use order of mentioning requests matters because it doesnt match url exactly
+  console.log('In second /');
+  res.send('Hello from second /');
+  // next(); // without next we dont move to the second app.use function
 });
 
 /*
