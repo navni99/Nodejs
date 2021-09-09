8575ad1714ce5c03c70f7bebb692f1b4f892dc80
navni99 <50635765+navni99@users.noreply.github.com>
2021-09-09 14:59:27 +0530

    adding routes

--- express/app.js
+++ express/app.js
@@ -1,19 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const app = express();
-app.use(bodyParser.urlencoded({ extended: false })); //no need to parse manually now
-app.get('/add-product', (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Submit</button></form>'
-  );
-});
-app.post('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-app.get('/', (req, res, next) => {
-  res.send('Hello from /');
-});
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(adminRoutes);
+app.use(shopRoutes); //app.use can be replaced with get if needed
+app.use('/', (req, res, next) => {
+  res.status(404).send('Page not found'); //sending status, setHeaders can be used like status
+});
 app.listen(3000);
